1c4a96ba8b41634d30f20d97e603ebee8fbebab7
Riya <riya.n@advanz101.com>
2021-04-10 21:05:29 +0530

    uploaded food receipe project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipeComponent } from './receipe/receipe.component';
+import { ReceipeListComponent } from './receipe/receipe-list/receipe-list.component';
+import { ReceipeItemComponent } from './receipe/receipe-item/receipe-item.component';
+import { ReceipeDetailComponent } from './receipe/receipe-detail/receipe-detail.component';
+import { ShoppinggListComponent } from './shoppingg-list/shoppingg-list.component';
+import { ShoppingListEditComponent } from './shoppingg-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipeComponent,
+    ReceipeListComponent,
+    ReceipeItemComponent,
+    ReceipeDetailComponent,
+    ShoppinggListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shoppingg-list/shoppingg-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppingg-list',
+  templateUrl: './shoppingg-list.component.html',
+  styleUrls: ['./shoppingg-list.component.css']
+})
+export class ShoppinggListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
